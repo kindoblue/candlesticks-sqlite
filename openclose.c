28fe2b438ed8c84c88437b5cc504c7aa71fc7f30
Stefano S <kindoblue@gmail.com>
2018-02-25 18:45:04 +0100

    first cut implementation

--- openclose.c
+++ openclose.c
@@ -5,66 +5,107 @@
 #include <sqlite3.h>
 #include <sqlite3ext.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <stdbool.h>
 
 SQLITE_EXTENSION_INIT1
 
-typedef struct open_state_s {
-    double   total_data;  /* sum of (data * weight) values */
-    double   total_wt;    /* sum of weight values */
-} open_state;
+typedef struct open_close_values_S {
+    int     init_flag;
+    double  open_value;
+    double  close_value;
+    long    min_timestamp;
+    long    max_timestamp;
+} open_close_values;
 
 
-void open_step(sqlite3_context *ctx, int num_values, sqlite3_value **values)
+void min_max_step(sqlite3_context *ctx, int num_values, sqlite3_value **values)
 {
-    double         row_wt = 1.0;
+    long  timestamp = 0;
     int            type = 0;
+    bool           is_first = false;
+    bool           is_last = false;
 
-    open_state   *st = (open_state*)sqlite3_aggregate_context( ctx,
-                                                                   sizeof( open_state ) );
+    open_close_values   *st = (open_close_values*)sqlite3_aggregate_context( ctx,
+                                                                   sizeof( open_close_values ) );
     if ( st == NULL ) {
         sqlite3_result_error_nomem( ctx );
         return;
     }
+
+    // initialize the context the first time
+    if ( st->init_flag == 0 ) {
+        st->init_flag = 1;
+        st->min_timestamp = LONG_MAX;
+        st->max_timestamp = LONG_MIN;
+     }
 
     /* Extract weight, if we have a weight and it looks like a number */
-    if ( num_values == 1 ) {
+    if ( num_values == 2 ) {
         type = sqlite3_value_numeric_type( values[1] );
-        if ( ( type == SQLITE_FLOAT )||( type == SQLITE_INTEGER ) ) {
-            row_wt = sqlite3_value_double( values[1] );
+        if (type == SQLITE_INTEGER ) {
+            timestamp = sqlite3_value_int64( values[1] );
+
+            if (timestamp < st->min_timestamp) {
+                is_first = true;
+                st->min_timestamp = timestamp;
+            }
+
+            if (timestamp > st->max_timestamp) {
+                is_last = true;
+                st->max_timestamp = timestamp;
+            }
+
+        } else {
+            // TODO error
+        }
+    } else {
+        // TODO error
+    }
+
+    // save the price value in case this record is
+    // eligible as first or last record
+    if (is_first || is_last) {
+
+        type = sqlite3_value_numeric_type(values[0]);
+
+        if (type == SQLITE_FLOAT) {
+            double price = sqlite3_value_double(values[0]);
+            if (is_first) {
+                st->open_value = price;
+            } else if (is_last) {
+                st->close_value = price;
+            }
+        } else {
+            // TODO error
         }
     }
-
-    /* Extract data, if we were given something that looks like a number. */
-    type = sqlite3_value_numeric_type( values[0] );
-    if ( ( type == SQLITE_FLOAT )||( type == SQLITE_INTEGER ) ) {
-       st->total_data += row_wt * sqlite3_value_double( values[0] );
-       st->total_wt   += row_wt;
-    }
 }
 
-void close_step(sqlite3_context *ctx, int num_values, sqlite3_value **values)
-{
-
-}
 void open_final(sqlite3_context *ctx)
 {
-    double         result = 0.0;
-    open_state   *st = (open_state*)sqlite3_aggregate_context( ctx,
-                                                                   sizeof( open_state ) );
+    open_close_values   *st = (open_close_values*)sqlite3_aggregate_context( ctx,
+                                                                   sizeof( open_close_values ) );
     if ( st == NULL ) {
         sqlite3_result_error_nomem( ctx );
         return;
     }
 
-    if ( st->total_wt != 0.0 ) {
-        result = st->total_data / st->total_wt;
-    }
-    sqlite3_result_double( ctx, result );
+    // setting the aggregate result
+    sqlite3_result_double( ctx, st->open_value );
 }
 
 void close_final(sqlite3_context *ctx)
 {
+    open_close_values   *st = (open_close_values*)sqlite3_aggregate_context( ctx,
+                                                                             sizeof( open_close_values ) );
+    if ( st == NULL ) {
+        sqlite3_result_error_nomem( ctx );
+        return;
+    }
 
+    // setting the aggregate result
+    sqlite3_result_double( ctx, st->close_value );
 }
 
 int sqlite3_candlestick_init(
@@ -77,11 +118,11 @@ int sqlite3_candlestick_init(
     SQLITE_EXTENSION_INIT2(pApi);
 
     // register the aggregate open/close functions
-    rc += sqlite3_create_function(db, "candlestick_open", 1, SQLITE_UTF8, NULL,
-                                  NULL, open_step, open_final);
+    rc += sqlite3_create_function(db, "candlestick_open", 2, SQLITE_UTF8, NULL,
+                                  NULL, min_max_step, open_final);
 
-    rc += sqlite3_create_function(db, "candlestick_close", 1, SQLITE_UTF8, NULL,
-                                  NULL, close_step, close_final);
+    rc += sqlite3_create_function(db, "candlestick_close", 2, SQLITE_UTF8, NULL,
+                                  NULL, min_max_step, close_final);
 
     return rc;
 }
